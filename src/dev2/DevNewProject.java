9696fbcf3fdffb8922def43d9d84d956069291ca
Roj2021 <botani2019@gmail.com>
2023-04-04 14:30:57 -0400

    day2 commit

--- src/dev2/DevNewProject.java
+++ src/dev2/DevNewProject.java
@@ -5,6 +5,7 @@ public class DevNewProject {
     public static void main(String[] args) {
 
         System.out.println("I want to go to visit India");
+        System.out.println("Pyrtyush is good man");
 
     }
 
