515c0d1b39f000a5337edab0af64f366097fc2d9
Ridvan <cadiroglu34@gmail.com>
2023-04-04 13:30:54 -0500

    Day 2 commit

--- src/dev1/DevProject.java
+++ src/dev1/DevProject.java
@@ -4,5 +4,6 @@ public class DevProject {
     public static void main(String[] args) {
 
         System.out.println("Boliwood is the best");
+        System.out.println("Prutyush gives priority to Tanya always");
     }
 }
